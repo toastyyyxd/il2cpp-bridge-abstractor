b6eee4fc6e5e6275dc2729c367918cf7a0350444
toasty <asaeasaeasae.x10@gmail.com>
2023-11-19 22:16:56 +0800

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import { AbstractedObject, AbstractedArray, Abstractor } from "./bridge/system/abstracted";
-import { getNativeInstanceUnsafe, getNativeInstance, checkEquality } from "./bridge/system/instanceHelper";
-import { List, Dictionary } from "./bridge/system/collections";
-import { UnityObject, GameObject, Component } from "./bridge/unity/objects";
-import { GameObjectHelper } from "./bridge/unity/gameObjectHelper";
-import { MonoBehaviour } from "./bridge/unity/monoBehaviour";
+import { AbstractedObject, AbstractedArray, Abstractor } from "./bridge/system/abstracted.js";
+import { getNativeInstanceUnsafe, getNativeInstance, checkEquality } from "./bridge/system/instanceHelper.js";
+import { isMainThreadReady } from "./bridge/system/threadHelper.js";
+import { List, Dictionary } from "./bridge/system/collections.js";
+import { UnityObject, GameObject, Component } from "./bridge/unity/objects.js";
+import { GameObjectHelper } from "./bridge/unity/gameObjectHelper.js";
+import { MonoBehaviour } from "./bridge/unity/monoBehaviour.js";
 
 export {
     AbstractedObject, AbstractedArray, Abstractor,
     getNativeInstanceUnsafe, getNativeInstance, checkEquality,
+    isMainThreadReady,
     List, Dictionary,
     UnityObject, GameObject, Component,
     GameObjectHelper,
