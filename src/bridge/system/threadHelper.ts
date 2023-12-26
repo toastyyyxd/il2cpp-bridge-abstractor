15ddde432a8a1ee3e198f34368b1245ee1c071ee
toasty <asaeasaeasae.x10@gmail.com>
2023-12-26 19:13:05 +0800

    Added fallback for threadHelper main thread check

--- src/bridge/system/threadHelper.ts
+++ src/bridge/system/threadHelper.ts
@@ -2,14 +2,31 @@ interface Thread extends Il2Cpp.Thread {
     tryLocalValue<T extends Il2Cpp.Object>(klass: Il2Cpp.Class): T | null;
 }
 
+let il2cpp_thread_get_all_attached_threadsStripped = false;
 export async function isMainThreadReady() : Promise<boolean> {
-    const thread = Il2Cpp.mainThread as Thread;
+    try {
+        const thread = Il2Cpp.mainThread as Thread;
+        const get_ExecutionContext = thread.object.tryMethod<Il2Cpp.Object>("GetMutableExecutionContext") ?? thread.object.method<Il2Cpp.Object>("get_ExecutionContext");
+        const executionContext = get_ExecutionContext.invoke();
 
-    const get_ExecutionContext = thread.object.tryMethod<Il2Cpp.Object>("GetMutableExecutionContext") ?? thread.object.method<Il2Cpp.Object>("get_ExecutionContext");
-    const executionContext = get_ExecutionContext.invoke();
-
-    let synchronizationContext = executionContext.tryField<Il2Cpp.Object>("_syncContext")?.value ??
-        executionContext.tryMethod<Il2Cpp.Object>("get_SynchronizationContext")?.invoke() ??
-        thread.tryLocalValue<Il2Cpp.Object>(Il2Cpp.corlib.class("System.Threading.SynchronizationContext"));
-    return synchronizationContext != null && !synchronizationContext.isNull();
+        let synchronizationContext = executionContext.tryField<Il2Cpp.Object>("_syncContext")?.value ??
+            executionContext.tryMethod<Il2Cpp.Object>("get_SynchronizationContext")?.invoke() ??
+            thread.tryLocalValue<Il2Cpp.Object>(Il2Cpp.corlib.class("System.Threading.SynchronizationContext"));
+        return synchronizationContext != null && !synchronizationContext.isNull();
+    } catch {
+        return await isMainThreadReadyFallback()
+    }
+}
+async function isMainThreadReadyFallback(): Promise<boolean> {
+    try {
+        Il2Cpp.perform(() => {
+            let i = 0;
+            i++;
+        }, 'main');
+    }
+    catch {
+        return false;
+    } finally {
+        return true;
+    }
 }
