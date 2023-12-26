51ae10d3018c66c5b8ff863315760a1a5b85deb4
toasty <asaeasaeasae.x10@gmail.com>
2023-12-26 19:18:19 +0800

    Hotfix: i forgot to build

--- dist/bridge/system/threadHelper.js
+++ dist/bridge/system/threadHelper.js
@@ -1,10 +1,30 @@
+let il2cpp_thread_get_all_attached_threadsStripped = false;
 export async function isMainThreadReady() {
-    const thread = Il2Cpp.mainThread;
-    const get_ExecutionContext = thread.object.tryMethod("GetMutableExecutionContext") ?? thread.object.method("get_ExecutionContext");
-    const executionContext = get_ExecutionContext.invoke();
-    let synchronizationContext = executionContext.tryField("_syncContext")?.value ??
-        executionContext.tryMethod("get_SynchronizationContext")?.invoke() ??
-        thread.tryLocalValue(Il2Cpp.corlib.class("System.Threading.SynchronizationContext"));
-    return synchronizationContext != null && !synchronizationContext.isNull();
+    try {
+        const thread = Il2Cpp.mainThread;
+        const get_ExecutionContext = thread.object.tryMethod("GetMutableExecutionContext") ?? thread.object.method("get_ExecutionContext");
+        const executionContext = get_ExecutionContext.invoke();
+        let synchronizationContext = executionContext.tryField("_syncContext")?.value ??
+            executionContext.tryMethod("get_SynchronizationContext")?.invoke() ??
+            thread.tryLocalValue(Il2Cpp.corlib.class("System.Threading.SynchronizationContext"));
+        return synchronizationContext != null && !synchronizationContext.isNull();
+    }
+    catch {
+        return await isMainThreadReadyFallback();
+    }
+}
+async function isMainThreadReadyFallback() {
+    try {
+        Il2Cpp.perform(() => {
+            let i = 0;
+            i++;
+        }, 'main');
+    }
+    catch {
+        return false;
+    }
+    finally {
+        return true;
+    }
 }
 //# sourceMappingURL=threadHelper.js.map
